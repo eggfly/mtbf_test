856075d24b1db9471310ef50eee161d031559fd3
wangmingming1 <wangmingming1@xiaomi.com>
2019-01-10 19:25:28 +0800

    bump version to version=0.22

--- mtbf_preparation.py
+++ mtbf_preparation.py
@@ -386,7 +386,7 @@ if __name__ == "__main__":
     # WARNING:
     # DO NOT RENAME THIS VARIABLE OR MANUALLY
     # UPDATE THE VALUE, IT SHALL BE UPDATED BY release.sh AUTOMATICALLY
-    __script_version = 0.21
+    __script_version = 0.22
 
     argv = sys.argv
     argc = len(argv)
